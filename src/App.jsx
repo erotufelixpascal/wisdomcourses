7a7a985e9ea045c1a0c96c41986c5068582a995e
FelixE <erotufelixpascal2@gmail.com>
2023-12-01 11:45:17 +0300

    Feature

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import {BrowserRouter, Route, Routes} from "react-router-dom"
 import Login from './Componets/Login';
 import Home from './Componets/Home';
 import Signup from './Componets/SignUp';
+import Course from './Pages/Admin/Course';
+import CourseSurvey from './Pages/User/CourseSurvey';
 
 function App() {
   return (
@@ -10,7 +12,9 @@ function App() {
       <Routes>
         <Route path='/' element ={<Login />}></Route>
         <Route path='/home' element ={<Home />}></Route>
-        <Route path='/signup' element ={<Signup />}>Login</Route>
+        <Route path='/signup' element ={<Signup />}></Route>
+        <Route path='/course' element ={<Course />}></Route>
+        <Route path='/coursesurvey' element ={<CourseSurvey/>}></Route>
       </Routes>
     </BrowserRouter>
   );
--- src/Pages/Admin/Course.jsx
+++ src/Pages/Admin/Course.jsx
@@ -2,7 +2,20 @@ import React from "react";
 
 function Course() {
     return ( 
-        <div>Course Questions</div>
+        <div>Course Questions
+            <form class="form-inline" action="/action_page.php">
+            <div class="form-group">
+                <label class="sr-only" >Considering your experience in a relationship, how likely would you be to recommend a friend or a colleague to take this survey to understand the health of their relationship?0 Very Unlikely to 10 Very Likely:</label>
+                <input type="text" class="form-control" />
+            </div>
+            <div class="form-group">
+                <label class="sr-only" for="pwd">How satisfied are you with your partner for the following in your relationship?</label>
+                <input type="text" class="form-control" />
+            </div>
+            
+            <button type="submit" class="btn btn-default">Submit</button>
+            </form>
+        </div>
      );
 }
 
