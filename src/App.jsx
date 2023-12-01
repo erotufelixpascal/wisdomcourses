1bf2b1069924a84626c21ebe968d9a4a0a27701c
FelixE <erotufelixpascal2@gmail.com>
2023-12-01 04:06:43 +0300

    Feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter, Route, Routes} from "react-router-dom"
+import Login from './Componets/Login';
+import Home from './Componets/Home';
 
 function App() {
   return (
-    <div >
-      
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/login' element ={<Login />}></Route>
+        <Route path='/' element ={<Home />}></Route>
+        {/* <Route path='/login' element ={<Login />}>Login</Route> */}
+      </Routes>
+    </BrowserRouter>
   );
 }
 
