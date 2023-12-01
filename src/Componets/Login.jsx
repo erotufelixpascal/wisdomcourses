9cadf6d7c8c0c563492a2cd42db04bd714315b75
FelixE <erotufelixpascal2@gmail.com>
2023-12-01 04:43:35 +0300

    Feature

--- src/Componets/Login.jsx
+++ src/Componets/Login.jsx
@@ -1,51 +1,30 @@
 import React from "react";
+import 'bootstrap/dist/css/bootstrap.css';
+import './Login.css';
 
 function Login() {
     return ( 
-        <div>
-            <div class="card mb-3">
-            <div class="row g-0 d-flex align-items-center">
-            <div class="col-lg-4 d-none d-lg-flex">
-                <img src="https://mdbootstrap.com/img/new/ecommerce/vertical/004.jpg" alt="Trendy Pants and Shoes"
-                class="w-100 rounded-t-5 rounded-tr-lg-0 rounded-bl-lg-5" />
-            </div>
-            <div class="col-lg-8">
-                <div class="card-body py-5 px-md-5">
-
-                <form>
-                    {/* <!-- Email input --> */}
-                    <div class="form-outline mb-4">
-                    <input type="email" id="form2Example1" class="form-control" />
-                    <label class="form-label" for="form2Example1">Email address</label>
-                    </div>
-                    <div class="form-outline mb-4">
-                    <input type="password" id="form2Example2" class="form-control" />
-                    <label class="form-label" for="form2Example2">Password</label>
-                    </div>
-                    <div class="row mb-4">
-                    <div class="col d-flex justify-content-center">
-                        {/* <!-- Checkbox --> */}
-                        <div class="form-check">
-                        <input class="form-check-input" type="checkbox" value="" id="form2Example31" checked />
-                        <label class="form-check-label" for="form2Example31"> Remember me </label>
-                        </div>
-                    </div>
-
-                    <div class="col">
-                        {/* <!-- Simple link --> */}
-                        <a href="#!">Forgot password?</a>
-                    </div>
-                    </div>
-
-                    {/* <!-- Submit button --> */}
-                    <button type="button" class="btn btn-primary btn-block mb-4">Sign in</button>
-
-                </form>
-            </div>
-            </div>
-        </div> 
+        <div class="back">
+        <div class="div-center">
+        <div class="content">
+            <form>
+                <div class="form-group">
+                <label for="exampleInputEmail1">Email address</label>
+                <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email"></input>
+                </div>
+                <div class="form-group">
+                <label for="exampleInputPassword1">Password</label>
+                <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password"></input>
+                </div>
+                <button type="submit" class="btn btn-primary">Login</button>
+                <hr />
+                <button type="button" class="btn btn-link">Signup</button>
+                <button type="button" class="btn btn-link">Reset Password</button>
+            </form>
         </div>
         </div>
+        </div>
+
         );
 }
 
